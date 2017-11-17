659dc6e444965ac0bae1bed39d81b4d162a6cd0a
Severiano Badajoz <sevebadajoz@gmail.com>
2017-11-16 18:40:11 -0800

    Create getUriFromResource method

--- app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/MainActivity.java
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/MainActivity.java
@@ -1,5 +1,9 @@
 package vhoang52.cs273.orangecoastcollege.edu.dotatracker;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -22,5 +26,14 @@ public class MainActivity extends AppCompatActivity {
         mTabs = findViewById(R.id.tabLayout);
         mTabs.setupWithViewPager(mViewPager);
 
+    }
+
+    public static Uri getUriFromResource(Context context, int resID) {
+        Resources res = context.getResources();
+        String uri = ContentResolver.SCHEME_ANDROID_RESOURCE + "://"
+                + res.getResourcePackageName(resID) + "/"
+                + res.getResourceTypeName(resID) + "/"
+                + res.getResourceEntryName(resID);
+        return Uri.parse(uri);
     }
 }
