f3da6ea5642eefc1f65604631bba381c20bfdba1
Severiano Badajoz <sevebadajoz@gmail.com>
2017-11-16 18:00:11 -0800

    Rename activity

--- app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/AccountActivity.java
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/AccountActivity.java
@@ -3,7 +3,7 @@ package vhoang52.cs273.orangecoastcollege.edu.dotatracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AccountActibity extends AppCompatActivity {
+public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
