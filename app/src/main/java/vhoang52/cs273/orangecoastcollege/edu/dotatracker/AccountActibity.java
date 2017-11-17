b0eb712e307571b2dc01b91573d3269b09c6b9a8
Severiano Badajoz <sevebadajoz@gmail.com>
2017-11-16 17:58:17 -0800

    Create an activity to control activity_seve_account

---
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/AccountActibity.java
@@ -0,0 +1,12 @@
+package vhoang52.cs273.orangecoastcollege.edu.dotatracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountActibity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
