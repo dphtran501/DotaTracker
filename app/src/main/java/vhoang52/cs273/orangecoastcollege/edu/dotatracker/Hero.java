1f5e350eb14eb36253bf7cff961e6951411b0fbf
Severiano Badajoz <sevebadajoz@gmail.com>
2017-11-20 14:58:35 -0800

    Create functionality of Hero class

--- app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/Hero.java
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/Hero.java
@@ -2,4 +2,37 @@ package vhoang52.cs273.orangecoastcollege.edu.dotatracker;
 
 
 public class Hero {
+    private String mHeroName;
+    private short mID;
+    private String mFileName;
+
+    public Hero(String heroName, short ID, String fileName) {
+        mHeroName = heroName;
+        mID = ID;
+        mFileName = "hero_" + ID;
+    }
+
+    public String getHeroName() {
+        return mHeroName;
+    }
+
+    public void setHeroName(String heroName) {
+        mHeroName = heroName;
+    }
+
+    public short getID() {
+        return mID;
+    }
+
+    public void setID(short ID) {
+        mID = ID;
+    }
+
+    public String getFileName() {
+        return mFileName;
+    }
+
+    public void setFileName(String fileName) {
+        mFileName = fileName;
+    }
 }
