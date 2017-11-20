0ffd940b065a7f6425464515284f37eddf4fce02
Severiano Badajoz <sevebadajoz@gmail.com>
2017-11-20 15:07:44 -0800

    Create and connect adapter to list

--- app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/AccountActivity.java
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/AccountActivity.java
@@ -7,18 +7,28 @@ import android.widget.TextView;
 import de.hdodenhof.circleimageview.CircleImageView;
 import io.netopen.hotbitmapgg.library.view.RingProgressBar;
 
+import java.util.List;
+
 public class AccountActivity extends AppCompatActivity {
+
+    private CircleImageView profilePicture;
+    private TextView userName;
+    private RingProgressBar winRingProgressBar;
+    private RingProgressBar lossRingProgressBar;
+    private ListView mostPlayedHeroesListView;
+    private MostPlayedHeroesListAdapter listAdapter;
+    private List<Hero> mMostPlayedHeros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        CircleImageView profilePicture = findViewById(R.id.profilePicture);
-        TextView userName = findViewById(R.id.userName);
-        RingProgressBar winRingProgressBar = findViewById(R.id.winRingProgressBar);
-        RingProgressBar lossRingProgressBar = findViewById(R.id.lossRingProgressBar);
-        ListView mostPlayedHeroesListView = findViewById(R.id.mostPlayedHeroesListView);
-
-
+        profilePicture = findViewById(R.id.profilePicture);
+        userName = findViewById(R.id.userName);
+        winRingProgressBar = findViewById(R.id.winRingProgressBar);
+        lossRingProgressBar = findViewById(R.id.lossRingProgressBar);
+        mostPlayedHeroesListView = findViewById(R.id.mostPlayedHeroesListView);
+        listAdapter = new MostPlayedHeroesListAdapter(this, R.layout.hero_list_item, mMostPlayedHeros);
+        mostPlayedHeroesListView.setAdapter(listAdapter);
     }
 }
