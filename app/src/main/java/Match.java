9060a1fe141c1991bd2053840ebf97a1ae720576
Derek <dphtran501@yahoo.com>
2017-11-25 00:50:55 -0800

    * Add getters to Match class * Add WebAPI hyperlink in javadocs

--- app/src/main/java/Match.java
+++ app/src/main/java/Match.java
@@ -1,17 +1,140 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import vhoang52.cs273.orangecoastcollege.edu.dotatracker.Player;
+
 /**
  * This class represents a single DOTA match played by the user.
  *
  * @author Derek Tran
  * @version 1.0
+ * @see <a href="https://wiki.teamfortress.com/wiki/WebAPI/GetMatchDetails">
+ * https://wiki.teamfortress.com/wiki/WebAPI/GetMatchDetails</a>
  * @since November 23, 2017
  */
 
 public class Match
 {
+    // TODO: check if ID is integer or string
     private String mID;
-    private int duration;
-    private int lastPlayed;
-    private String winner;
+    private int mMatchSequenceNumber;
+    private List<Player> mPlayersList;
+    private boolean mIsRadiantWin;
+    private int mDuration;              // seconds
+    private int mStartTime;             // Unix timestamp
+    // TODO: Lobby type?
+    // TODO: Game mode?
+    private int mRadiantScore;
+    private int mDireScore;
 
+    /**
+     * Instantiates a <code>Match</code> object given the match's statistics.
+     *
+     * @param ID                  The ID of the match.
+     * @param matchSequenceNumber The sequence number representing the order in which the match was
+     *                            recorded in the match history.
+     * @param playersList         The list of players in the match.
+     * @param isRadiantWin        Truth value of whether Radiant won the match.
+     * @param duration            The length of the match in seconds.
+     * @param startTime           Unix timestamp of when the match began.
+     * @param radiantScore        Radiant's score.
+     * @param direScore           Dire's score.
+     */
+    public Match(String ID, int matchSequenceNumber, List<Player> playersList, boolean isRadiantWin, int duration, int startTime, int radiantScore, int direScore)
+    {
+        mID = ID;
+        mMatchSequenceNumber = matchSequenceNumber;
+        mPlayersList = playersList;
+        mIsRadiantWin = isRadiantWin;
+        mDuration = duration;
+        mStartTime = startTime;
+        mRadiantScore = radiantScore;
+        mDireScore = direScore;
+    }
 
+    /**
+     * Gets the ID of this match.
+     *
+     * @return The ID of this match.
+     */
+    public String getID() { return mID; }
+
+    /**
+     * Gets the sequence number representing the order in which the match was recorded in the match
+     * history.
+     *
+     * @return The sequence number representing the order in which the match was recorded in the
+     * match history.
+     */
+    public int getMatchSequenceNumber() { return mMatchSequenceNumber; }
+
+    /**
+     * Gets the list of <code>Player</code>s that were in this match.
+     *
+     * @return The list of <code>Player</code>s that were in this match.
+     */
+    public List<Player> getPlayersList() { return mPlayersList; }
+
+    /**
+     * Checks whether Radiant won this match. True if Radiant won; false if Dire won.
+     *
+     * @return True if Radiant won; false if Dire won.
+     */
+    public boolean isRadiantWin() { return mIsRadiantWin; }
+
+    /**
+     * Gets the duration of this match in seconds.
+     *
+     * @return The duration of this match in seconds.
+     */
+    public int getDuration() { return mDuration; }
+
+    /**
+     * Gets the Unix timestamp of when this match began.
+     *
+     * @return The Unix timestamp of when this match began.
+     */
+    public int getStartTime() { return mStartTime; }
+
+    /**
+     * Gets Radiant's score.
+     *
+     * @return Radiant's score.
+     */
+    public int getRadiantScore() { return mRadiantScore; }
+
+    /**
+     * Gets Dire's score.
+     *
+     * @return Dire's score.
+     */
+    public int getDireScore() { return mDireScore; }
+
+    /**
+     * Gets the list of <code>Player</code>s that were on team Radiant.
+     *
+     * @return The list of <code>Player</code>s that were on team Radiant.
+     */
+    public List<Player> getRadiantPlayers()
+    {
+        List<Player> radiantPlayers = new ArrayList<>();
+        for (Player player : mPlayersList)
+            if (!player.isDire()) radiantPlayers.add(player);
+
+        return radiantPlayers;
+    }
+
+    /**
+     * Gets the list of <code>Player</code>s that were on team Dire.
+     *
+     * @return The list of <code>Player</code>s that were on team Dire.
+     */
+    public List<Player> getDirePlayers()
+    {
+        List<Player> direPlayers = new ArrayList<>();
+        for (Player player : mPlayersList)
+            if (player.isDire()) direPlayers.add(player);
+
+        return direPlayers;
+    }
 }
--- app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/Player.java
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/Player.java
@@ -5,6 +5,8 @@ package vhoang52.cs273.orangecoastcollege.edu.dotatracker;
  *
  * @author Derek Tran
  * @version 1.0
+ * @see <a href="https://wiki.teamfortress.com/wiki/WebAPI/GetMatchDetails">
+ * https://wiki.teamfortress.com/wiki/WebAPI/GetMatchDetails</a>
  * @since November 23, 2017
  */
 
@@ -77,115 +79,115 @@ public class Player
     }
 
     /**
-     * Gets the account ID of the player.
+     * Gets the account ID of this player.
      *
-     * @return The account ID of the player.
+     * @return The account ID of this player.
      */
     public int getAccountID() { return mAccountID; }
 
     /**
-     * Gets the 8-bit unsigned integer where the first bit represents the player's team and the
-     * final three bits represent the player's position in the team.
+     * Gets the 8-bit unsigned integer where the first bit represents this player's team and the
+     * final three bits represent this player's position in the team.
      *
-     * @return The 8-bit unsigned integer of the player.
+     * @return The 8-bit unsigned integer of this player.
      */
     public byte getPlayerSlot() { return mPlayerSlot; }
 
     /**
-     * Gets the ID of the hero played by the player in the match.
+     * Gets the ID of the hero played by this player in the match.
      *
-     * @return The ID of the hero played by the player in the match.
+     * @return The ID of the hero played by this player in the match.
      */
     public int getHeroID() { return mHeroID; }
 
     /**
-     * Gets the number of kills by the player in the match.
+     * Gets the number of kills by this player in the match.
      *
-     * @return The number of kills by the player in the match.
+     * @return The number of kills by this player in the match.
      */
     public int getKills() { return mKills; }
 
     /**
-     * Gets the number of deaths of the player in the match.
+     * Gets the number of deaths of this player in the match.
      *
-     * @return The number of deaths of the player in the match.
+     * @return The number of deaths of this player in the match.
      */
     public int getDeaths() { return mDeaths; }
 
     /**
-     * Gets the number of assists by the player in the match.
+     * Gets the number of assists by this player in the match.
      *
-     * @return The number of assists by the player in the match.
+     * @return The number of assists by this player in the match.
      */
     public int getAssists() { return mAssists; }
 
     /**
-     * Gets the amount of gold the player had by the end of the match.
+     * Gets the amount of gold this player had by the end of the match.
      *
-     * @return The amount of gold the player had by the end of the match.
+     * @return The amount of gold this player had by the end of the match.
      */
     public int getGold() { return mGold; }
 
     /**
-     * Gets the number of last hits by the player in the match.
+     * Gets the number of last hits by this player in the match.
      *
-     * @return The number of last hits by the player in the match.
+     * @return The number of last hits by this player in the match.
      */
     public int getLastHits() { return mLastHits; }
 
     /**
-     * Gets the number of denies by the player in the match.
+     * Gets the number of denies by this player in the match.
      *
-     * @return The number of denies by the player in the match.
+     * @return The number of denies by this player in the match.
      */
     public int getDenies() { return mDenies; }
 
     /**
-     * Gets the average amount of gold the player gained per minute in the match.
+     * Gets the average amount of gold this player gained per minute in the match.
      *
-     * @return The average amount of gold the player gained per minute in the match.
+     * @return The average amount of gold this player gained per minute in the match.
      */
     public int getGPM() { return mGPM; }
 
     /**
-     * Gets the average amount of experience the player gained per minute in the match.
+     * Gets the average amount of experience this player gained per minute in the match.
      *
-     * @return The average amount of experience the player gained per minute in the match.
+     * @return The average amount of experience this player gained per minute in the match.
      */
     public int getXPM() { return mXPM; }
 
     /**
-     * Gets the amount of damage the player dealt to heroes in the match.
+     * Gets the amount of damage this player dealt to heroes in the match.
      *
-     * @return The amount of damage the player dealt to heroes in the match.
+     * @return The amount of damage this player dealt to heroes in the match.
      */
     public int getHeroDamage() { return mHeroDamage; }
 
     /**
-     * Gets the amount of damage the player dealt to towers in the match.
+     * Gets the amount of damage this player dealt to towers in the match.
      *
-     * @return The amount of damage the player dealt to towers in the match.
+     * @return The amount of damage this player dealt to towers in the match.
      */
     public int getTowerDamage() { return mTowerDamage; }
 
     /**
-     * Gets the amount of health the player healed on heroes in the match.
+     * Gets the amount of health this player healed on heroes in the match.
      *
-     * @return The amount of health the player healed on heroes in the match.
+     * @return The amount of health this player healed on heroes in the match.
      */
     public int getHeroHealing() { return mHeroHealing; }
 
     /**
-     * Gets the level of the player by the end of the match.
+     * Gets the level of this player by the end of the match.
      *
-     * @return The level of the player by the end of the match.
+     * @return The level of this player by the end of the match.
      */
     public int getLevel() { return mLevel; }
 
     /**
-     * Checks whether the player is on team Dire or team Radiant.
+     * Checks whether this player is on team Dire or team Radiant.
      *
-     * @return True if the player is on team Dire; false if on team Radiant.
+     * @return True if this player is on team Dire; false if on team Radiant.
      */
     public boolean isDire()
     {
