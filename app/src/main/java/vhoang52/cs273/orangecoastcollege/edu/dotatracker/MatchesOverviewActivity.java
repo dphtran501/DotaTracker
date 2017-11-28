b7d6f5b9cc58b90623155b61f4c11cd35d616154
Derek <dphtran501@yahoo.com>
2017-11-28 10:57:34 -0800

    Connect to widgets of activity_matches_overview layout in MatchesOverviewActivity class

--- app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/MatchesOverviewActivity.java
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/MatchesOverviewActivity.java
@@ -1,5 +1,8 @@
 package vhoang52.cs273.orangecoastcollege.edu.dotatracker;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -66,8 +69,56 @@ public class MatchesOverviewActivity extends AppCompatActivity
         // Connect to database
         db = new DBHelper(this);
 
-        // Connect to widgets
-        // TODO: add widgets
+        // Connect to player profile widgets
+        playerImageView = (ImageView) findViewById(R.id.playerImageView);
+        playerNameTextView = (TextView) findViewById(R.id.playerNameTextView);
+        // Connect to player stats widgets
+        winsTextView = (TextView) findViewById(R.id.winsTextView);
+        lossesTextView = (TextView) findViewById(R.id.lossesTextView);
+        winRateTextView = (TextView) findViewById(R.id.winRateTextView);
+        avgWinRateTextView = (TextView) findViewById(R.id.avgWinRateTextView);
+        avgKillsTextView = (TextView) findViewById(R.id.avgKillsTextView);
+        avgDeathsTextView = (TextView) findViewById(R.id.avgDeathsTextView);
+        avgAssistsTextView = (TextView) findViewById(R.id.avgAssistsTextView);
+        avgGPMTextView = (TextView) findViewById(R.id.avgGPMTextView);
+        avgXPMTextView = (TextView) findViewById(R.id.avgXPMTextView);
+        avgLastHitsTextView = (TextView) findViewById(R.id.avgLastHitsTextView);
+        avgHeroDMGTextView = (TextView) findViewById(R.id.avgHeroDMGTextView);
+        avgHeroHealTextView = (TextView) findViewById(R.id.avgHeroHealTextView);
+        avgTowerDMGTextView = (TextView) findViewById(R.id.avgTowerDMGTextView);
+        avgDurationTextView = (TextView) findViewById(R.id.avgDurationTextView);
+        // Connect to label widgets
+        averagesLabelTextView = (TextView) findViewById(R.id.averagesLabelTextView);
+        recentMatchesTextView = (TextView) findViewById(R.id.recentMatchesTextView);
+        // Connect to ListView
+        matchList = db.getAllMatches();
+        matchListAdapter = new MatchListAdapter(this, R.layout.match_list_item, matchList);
+        matchListView.setAdapter(matchListAdapter);
+
+        // Set default player profile image
+        playerImageURI = getURIFromResource(this, R.drawable.steam_icon);
+        playerImageView.setImageURI(playerImageURI);
 
     }
+
+    /**
+     * Get URI to any resource type within an Android Studio project. Method is public static to
+     * allow other classes to use it as a helper function.
+     *
+     * @param context The current context
+     * @param resID   The resource identifier of the drawable
+     * @return Uri to resource by given ID
+     * @throws Resources.NotFoundException If the given ID does not exist.
+     */
+    public static Uri getURIFromResource(Context context, int resID) throws Resources.NotFoundException
+    {
+        Resources res = context.getResources();
+        // Build a String in the form:
+        // android.resource://edu.orangecoastcollege.cs273.petprotector/drawable/none
+        String uri = ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + res.getResourcePackageName(resID)
+                + "/" + res.getResourceTypeName(resID) + "/" + res.getResourceEntryName(resID);
+        // Parse to Uri object
+        return Uri.parse(uri);
+    }
+
 }
