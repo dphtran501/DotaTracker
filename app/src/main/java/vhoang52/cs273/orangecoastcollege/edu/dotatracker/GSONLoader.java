dcc19f3a37cfebd043267a648222710746ed66ca
Derek <dphtran501@yahoo.com>
2017-12-02 23:17:55 -0800

    Add (unfinished) GSONLoader class

---
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/GSONLoader.java
@@ -0,0 +1,13 @@
+package vhoang52.cs273.orangecoastcollege.edu.dotatracker;
+
+/**
+ * A class to unpack JSON files and load their information into the application's data models.
+ *
+ * @author Derek Tran
+ * @version 1.0
+ * @since December 2, 2017
+ */
+public class GSONLoader
+{
+    // TODO: Implementation of GSONLoader class
+}
