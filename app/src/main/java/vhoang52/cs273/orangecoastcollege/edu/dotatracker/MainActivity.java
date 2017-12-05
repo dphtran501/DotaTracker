33f7d7b00c4ce350010fbb2c3f3e04dc9a8388cb
Severiano Badajoz <sevebadajoz@gmail.com>
2017-12-04 23:46:44 -0800

    Create launch method for FluffActivity

--- app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/MainActivity.java
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/MainActivity.java
@@ -2,18 +2,23 @@ package vhoang52.cs273.orangecoastcollege.edu.dotatracker;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TabLayout mTabs;
     ViewPager mViewPager;
     MainActivityPagerAdapter mMainActivityPagerAdapter;
+    private long startMillis;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +42,23 @@ public class MainActivity extends AppCompatActivity {
                 + res.getResourceTypeName(resID) + "/"
                 + res.getResourceEntryName(resID);
         return Uri.parse(uri);
+    }
+
+    public void tapProfilePicture(View view) {
+
+
+        long time = System.currentTimeMillis();
+
+
+        if (startMillis == 0 || (time - startMillis > 3000)) {
+            startMillis = time;
+            count = 1;
+        } else {
+            count++;
+        }
+
+        if (count == 5) {
+            startActivity(new Intent(this, FluffActivity.class));
+        }
     }
 }
