30927a2efd46d8154b05436412b0ec2c3e74a713
Vincent Hoang <vietnhoang7@gmail.com>
2017-12-06 03:03:29 -0800

    finish HTTPRequestService and LoginActivity

--- app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/HTTPRequestService.java
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/HTTPRequestService.java
@@ -8,6 +8,7 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
@@ -24,32 +25,70 @@ public class HTTPRequestService
     // TODO: Implementation of GSONLoader class
     private static final String TAG = "GSONLoader";
     private static Gson gson = new Gson();
+    private static String BASE_URL = "http://68.4.78.45:8080/dotaweb/";
+
+    // User objects
+
+    public static boolean postUserID(long steamId64) {
+        String url = BASE_URL + "register?steamId64=" + steamId64;
+        AsyncHttpClient client = new AsyncHttpClient();
+        final List<Integer> whyAmIDoingItThisWay = new ArrayList<>();
+        client.post(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                whyAmIDoingItThisWay.add(1);
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                whyAmIDoingItThisWay.clear();
+            }
+        });
+
+        return (whyAmIDoingItThisWay.size() > 0);
+    }
+
+    public static List<User> getUserSummaries(long steamId64) {
+        String url = BASE_URL + "fetch/playersummary?steamId64=" + steamId64;
+        final List<User> userList = new ArrayList<>();
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                List<User> tempList = gson.fromJson(new String(bytes), new TypeToken<List<User>>(){}.getType());
+                userList.addAll(tempList);
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                Log.d(TAG, "Error retrieving user json", throwable);
+            }
+        });
+
+        return userList;
+    }
+
+    // Match objects
 
     public static List<Match> getMatchDetails(long steamId32) {
-        String url = "http://68.4.78.45:8080/dotaweb/fetch/refresh?steamId32=" + steamId32;
+        String url = BASE_URL + "fetch/refresh?steamId32=" + steamId32;
 
-        return getMatchDetails(steamId32);
-    }
-
-    private List<Match> returnMatchDetails(String responseBody) {
-        return gson.fromJson(responseBody, new TypeToken<List<Match>>(){}.getType());
-    }
-
-    private void getMatchJSON(String url) {
+        final List<Match> matchList = new ArrayList<>();
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(url, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                returnMatchDetails(responseBody.toString());
+                List<Match> tempList = gson.fromJson(new String(responseBody), new TypeToken<List<Match>>() {}.getType());
+                matchList.addAll(tempList);
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                 Log.d(TAG, "Error retrieving match json", error);
-                return;
             }
         });
-    }
 
+        return matchList;
+    }
     
 }
--- app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/LoginActivity.java
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/LoginActivity.java
@@ -1,18 +1,51 @@
 package vhoang52.cs273.orangecoastcollege.edu.dotatracker;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText mSteamIdEditText;
+    Button mRegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mSteamIdEditText = (EditText) findViewById(R.id.steamIDEditText);
+        mRegisterButton = (Button) findViewById(R.id.submitButton);
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitButton();
+            }
+        });
     }
 
+    private void submitButton() {
+
+        long steamId = Long.parseLong(mSteamIdEditText.getText().toString());
+        boolean success = HTTPRequestService.postUserID(steamId);
+
+        if (success) {
+            SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putLong("currentUserId", steamId);
+            Toast.makeText(this, "User updated", Toast.LENGTH_SHORT).show();
+            finish();
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Failed to retrieve user information", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
