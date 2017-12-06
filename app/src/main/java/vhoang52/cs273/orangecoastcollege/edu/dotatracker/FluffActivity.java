5c7bad159a7a92a85b4a99e623c15fc48b113a45
Severiano Badajoz <sevebadajoz@gmail.com>
2017-12-05 17:47:45 -0800

    Create MapsActivity.java and intent from FluffActivity

--- app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/FluffActivity.java
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/FluffActivity.java
@@ -2,6 +2,7 @@ package vhoang52.cs273.orangecoastcollege.edu.dotatracker;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.GnssStatus;
 import android.location.LocationManager;
@@ -14,16 +15,19 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class FluffActivity extends AppCompatActivity {
-
     public static final int LOCATION_REQUEST_CODE = 420;
+
+    private Button locationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fluff);
+        locationButton = findViewById(R.id.locationButton);
     }
 
     public void checkEarthquake(View view) {
@@ -31,21 +35,26 @@ public class FluffActivity extends AppCompatActivity {
     }
 
     public void checkLocation(View view) {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this
-                    , new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}
-                    , LOCATION_REQUEST_CODE);
-            checkEarthquake(view);
-        }
-        else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                LocationManager locationManager = ((LocationManager) getSystemService(LOCATION_SERVICE));
-                    if(locationManager.getAllProviders().size() > 0)
+        if (!locationButton.getText().equals("Are you sure?")) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this
+                        , new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}
+                        , LOCATION_REQUEST_CODE);
+                checkEarthquake(view);
+            } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    LocationManager locationManager = ((LocationManager) getSystemService(LOCATION_SERVICE));
+                    if (locationManager.getAllProviders().size() > 0) {
                         Toast.makeText(this, "You're on Earth, congrats", Toast.LENGTH_SHORT).show();
+                        locationButton.setText("Are you sure?");
+                    }
+                }
             }
+        } else {
+            startActivity(new Intent(this, MapsActivity.class));
         }
     }
 }
