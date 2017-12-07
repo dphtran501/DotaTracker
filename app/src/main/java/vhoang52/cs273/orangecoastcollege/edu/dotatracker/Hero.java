f54221094afb370f24808530e2893021c32a951b
Severiano Badajoz <sevebadajoz@gmail.com>
2017-12-07 02:55:52 -0800

    Create getallHeroes method

--- app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/Hero.java
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/Hero.java
@@ -1,6 +1,14 @@
 package vhoang52.cs273.orangecoastcollege.edu.dotatracker;
 
 
+import android.content.Context;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 public class Hero {
     private String mLocalizedName;
     private String mTokenName;
@@ -12,6 +20,18 @@ public class Hero {
         this.mTokenName = mTokenName;
         this.mID = mID;
         this.mFileName = "npc_dota_" + mTokenName + ".png";
+    }
+
+    public static List<Hero> getAllHeroes(Context context) throws IOException {
+        InputStream is = context.getAssets().open("heroes.json");
+        int size = is.available();
+        byte[] buffer = new byte[size];
+        is.read(buffer);
+        is.close();
+        Gson gson = new Gson();
+        return gson.fromJson(new String(buffer),
+                new TypeToken<List<Hero>>() {
+                }.getType());
     }
 
     public String getHeroName() {
