99efdf57676225432e3b73828cad7523b97c48b7
Severiano Badajoz <sevebadajoz@gmail.com>
2017-12-07 03:44:24 -0800

    Remove fileName field and setter

--- app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/Hero.java
+++ app/src/main/java/vhoang52/cs273/orangecoastcollege/edu/dotatracker/Hero.java
@@ -14,13 +14,12 @@ public class Hero {
     private String mLocalizedName;
     private String mTokenName;
     private int mId;
-    private String mFileName;
+
 
     public Hero(String mLocalizedName, String mTokenName, int mId) {
         this.mLocalizedName = mLocalizedName;
         this.mTokenName = mTokenName;
         this.mId = mId;
-        this.mFileName = "npc_dota_" + mTokenName + ".png";
     }
 
     public static List<Hero> getAllHeroes(Context context) throws IOException {
@@ -30,11 +29,9 @@ public class Hero {
         is.read(buffer);
         is.close();
         Gson gson = new Gson();
-        List<Hero> heroList = gson.fromJson(new String(buffer),
+        return gson.fromJson(new String(buffer),
                 new TypeToken<List<Hero>>() {
                 }.getType());
-
-        return heroList;
     }
 
     public static Hero getHeroFromID(Context context, int id) throws IOException {
@@ -62,12 +59,9 @@ public class Hero {
     }
 
     public String getFileName() {
-        return mFileName;
+        return "npc_dota_" + mTokenName + ".png";
     }
 
-    public void setFileName() {
-        mFileName = "npc_dota_" + mTokenName + ".png";
-    }
 
     @Override
     public String toString() {
@@ -75,7 +69,7 @@ public class Hero {
                 "mLocalizedName='" + mLocalizedName + '\'' +
                 ", mTokenName='" + mTokenName + '\'' +
                 ", mId=" + mId +
-                ", mFileName='" + mFileName + '\'' +
+                ", mFileName='" + getFileName() + '\'' +
                 '}';
     }
 }
